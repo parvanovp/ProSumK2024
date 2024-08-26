8026891bf677c75f6890d090eef1234a91ac8099
tommycws2018 <tommycws2018@gmail.com>
2024-08-26 12:50:26 +0300

    test

---
+++ git_proj003.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+void printboard(string a){
+    cout<<a<<endl;
+}
+
+
+
+int main() 
+{
+
+cout<<"Hi!"<<endl;
+
+printboard("Hello there!");
+
+
+
+
+
+return 1;
+
+
+}
